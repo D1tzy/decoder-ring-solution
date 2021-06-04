2ecc0e31a06d9859532ea9ee5c82858f70a182c5
Hayden Quale <haydencq@icloud.com>
2021-06-04 18:07:25 -0400

    commit to try and link qualified

--- src/caesar.js
+++ src/caesar.js
@@ -5,14 +5,53 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
+  const alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+   
+  }
+
+  function getCaesar(alphabet, shift, encode = true) {
+    try {
+      const returnAlphabet = [];
+
+      for (let each in alphabet) {
+        const letter = alphabet[each];
+        const index = each;
+
+        if ((shift > 0 && encode === true) || (shift < 0 && encode === false)) {
+          try {
+            letter = alphabet[index + Math.abs(shift)];
+            returnAlphabet.push(letter);
+          } catch (errors) {
+            const newValue = (index + Math.abs(shift)) - alphabet.length;
+            letter = alphabet[newValue];
+            returnAlphabet.push(letter); 
+          }
+
+        } else {
+          try {
+            letter = alphabet[index - Math.abs(shift)];
+            returnAlphabet.push(letter);
+          } catch (errors) {
+            const newValue = (alphabet.length - Math.abs(shift));
+            letter = alphabet[newValue];
+            returnAlphabet.push(letter);
+          }
+        }
+      }
+
+      return returnAlphabet;
+    } catch (error) {
+
+    }
   }
 
   return {
-    caesar,
+    caesar, getCaesar
   };
 })();
 
-module.exports = { caesar: caesarModule.caesar };
+module.exports = [{ caesar: caesarModule.caesar },
+                  { getCaesar: caesarModule.getCaesar} ];
